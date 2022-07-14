089cf130d77cb1c1e42fc7ca0c317285c9e2c89d
puspendu <puspendu09@gmail.com>
2022-07-15 00:13:21 +0530

    HasPath algorithm added

---
+++ Graph/HasPath.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+# depth first approach fo find the path if exist
+# def has_path(input_graph, src, tgt):
+#     if src == tgt:
+#         return True
+#
+#     for neighbour in graph[src]:
+#         if has_path(input_graph, neighbour, tgt):
+#             return True
+#
+#     return False
+queue = deque()
+
+
+def has_path(input_graph, src, tgt):
+    # if src == tgt:
+    #     return True
+    queue.append(src)
+    while len(queue) > 0:
+        current = queue.popleft()
+        if current == tgt:
+            return True
+        for neighbour in input_graph[current]:
+            queue.append(neighbour)
+
+    else:
+        return False
+
+
+graph = {
+    'a': ['b', 'c'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['f'],
+    'e': [],
+    'f': []
+}
+
+print(has_path(graph, 'b', 'f'))
